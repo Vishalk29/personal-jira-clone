436724b1bbd86f7b9e80f716f6c9c30d92b3f30f
Kulkarni Vishal <88853198+Vishalk29@users.noreply.github.com>
2025-09-27 22:03:38 +0530

    Created the signup form

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+"use-client";
+import { SignUpCard } from "@/features/auth/component/sign-up-card";
+import React from "react";
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
-}
+  return <SignUpCard/>
+};
 
 export default SignUpPage;
--- src/features/auth/component/sign-up-card.tsx
+++ src/features/auth/component/sign-up-card.tsx
@@ -1,22 +1,46 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
 import { DottedSaparator } from "@/components/dotted-saparator";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
-export const SignInCard = () => {
+import Link from "next/link";
+export const SignUpCard = () => {
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-7">
-        <CardTitle className="text-2xl">Welcome Back</CardTitle>
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          By signing up , you have agree to our{" "}
+          <Link href="/privacy">
+            <span className="text-blue-700">Privacy policy</span>
+          </Link>{" "}
+          and{" "}
+          <Link href="/term">
+            <span className="text-blue-700">Term of service</span>
+          </Link>{" "}
+        </CardDescription>
       </CardHeader>
       <div className="px-7">
         <DottedSaparator />
       </div>
       <CardContent className="p-7">
         <form className="space-y-4">
+          <Input
+            required
+            type="text"
+            value={""}
+            // onChange={() => {}}
+            placeholder="Enter your name"
+            disabled={false}
+          />
           <Input
             required
             type="email"
@@ -36,7 +60,7 @@ export const SignInCard = () => {
             max={256}
           />
           <Button className="w-full" size="lg">
-            Login
+            Register
           </Button>
         </form>
       </CardContent>
